bd6dd3a6625e595c03c8fb33aed8b8bde0d0a5f4
Jarrett Phillips <jphill01@uoguelph.ca>
2023-05-02 14:57:58 -0400

    Add files via upload

---
+++ summary_stats.R
@@ -0,0 +1,31 @@
+# Obtain minimum, mean, and maximum values of the metrics
+
+min(p_x, na.rm = TRUE)
+mean(p_x, na.rm = TRUE) 
+max(p_x, na.rm = TRUE)
+
+min(q_x, na.rm = TRUE)
+mean(q_x, na.rm = TRUE)
+max(q_x, na.rm = TRUE)
+
+min(p_x_prime_NN, na.rm = TRUE)
+mean(p_x_prime_NN, na.rm = TRUE)
+max(p_x_prime_NN, na.rm = TRUE)
+
+min(q_x_prime_NN, na.rm = TRUE)
+mean(q_x_prime_NN, na.rm = TRUE)
+max(q_x_prime_NN, na.rm = TRUE)
+
+# Obtain number of species attaining minimum and maximum values of the metrics
+
+length(which(na.omit(p_x) == min(p_x, na.rm = TRUE)))
+length(which(na.omit(p_x) == max(p_x, na.rm = TRUE)))
+
+length(which(na.omit(q_x) == min(q_x, na.rm = TRUE)))
+length(which(na.omit(q_x) == max(q_x, na.rm = TRUE)))
+
+length(which(na.omit(p_x_prime_NN) == min(p_x_prime_NN, na.rm = TRUE)))
+length(which(na.omit(p_x_prime_NN) == max(p_x_prime_NN, na.rm = TRUE)))
+
+length(which(na.omit(q_x_prime_NN) == min(q_x_prime_NN, na.rm = TRUE)))
+length(which(na.omit(q_x_prime_NN) == max(q_x_prime_NN, na.rm = TRUE)))
